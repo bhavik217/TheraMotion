37f08c6044c234366d67b1c7f81c3ca5352b4ba8
ARNAV GUPTA <arnavgupta1551@gmail.com>
2024-11-28 10:24:29 +0530

    Some file structure changes

--- FrontEnd/src/layout.jsx
+++ FrontEnd/src/layout.jsx
@@ -1,5 +1,5 @@
-import Navbar from "./components/elements/Navbar"
-import Footer from "./components/elements/Footer"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 import { Outlet } from "react-router-dom";
 
 function Layout(){
