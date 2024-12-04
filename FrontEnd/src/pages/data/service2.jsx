f181336a4de6c59854c047c7f0ba81b1098e5cb5
Bhavik Sharma <bhavik217.be22@chitkara.edu.in>
2024-12-04 22:42:54 +0530

    Created display data for BookAppt Page

---
+++ FrontEnd/src/pages/data/service2.jsx
@@ -0,0 +1,32 @@
+const data = [
+  {
+    name: "Classes",
+    arr: [
+      {
+        hdata: "Double Exercise Physiology Consultation",
+        time: "60 min"
+      },
+      {
+        hdata: "Single Exercise Physiology Consultation",
+        time: "30 min"
+      },
+
+    ]
+  },
+  {
+    name: "Pilates Classes",
+    arr: [
+      {
+        hdata: "Double Exercise Physiology Consultation",
+        time: "60 min"
+      },
+      {
+        hdata: "Single Exercise Physiology Consultation",
+        time: "30 min"
+      },
+
+    ]
+  },
+];
+
+export default data;
