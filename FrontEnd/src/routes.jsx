abf13993708b05251c1c47ee339fc7c48fbe0397
Bhavik Sharma <bhavik217.be22@chitkara.edu.in>
2024-12-04 22:52:05 +0530

    Added routes to new pages

--- FrontEnd/src/routes.jsx
+++ FrontEnd/src/routes.jsx
@@ -10,6 +10,12 @@ import Dashboard from "./pages/Dashboard";
 import MeetTeamLayout from "./pages/MeetTeamLayout";
 import PersonDetail from "./components/PersonDetails";
 import { Outlet, Navigate } from "react-router-dom";
+import BookAppt from "./pages/BookAppt.jsx";
+import Personselect from "./components/Personselect.jsx";
+import Calendar from "./pages/Calendar.jsx";
+import Finalform from "./pages/Finalform.jsx";
+import OrderDetail from "./pages/OrderDetail.jsx";
+import PersonSelectLayout from "./pages/PersonSelectLayout.jsx";
 
 const ProtectedRoute = () => {
     const authToken = localStorage.getItem("authToken");
@@ -71,8 +77,34 @@ export const routes = [
                     {
                         path: "/dashboard",
                         element: <Dashboard />,
+                    },
+                ]
+            },
+            {
+                path: "/book-appointment",
+                element: <BookAppt />,
+            },
+            {
+                path: "/person",
+                element: <PersonSelectLayout />,
+                children: [
+                    {
+                        index: true,
+                        element: <Personselect />,
+                    },
+                    {
+                        path: "calendar",
+                        element: <Calendar />,
                     }
                 ]
+            },
+            {
+                path: "/form",
+                element: <Finalform />,
+            },
+            {
+                path: "/order",
+                element: <OrderDetail />,
             },
         ]
     }
