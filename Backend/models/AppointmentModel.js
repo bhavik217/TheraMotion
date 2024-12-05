69b4baedfbda039fa63bd58adc0b404fced5c24c
ARNAV GUPTA <arnavgupta1551@gmail.com>
2024-12-06 01:14:33 +0530

    Fixed indentation in the code

--- Backend/models/AppointmentModel.js
+++ Backend/models/AppointmentModel.js
@@ -1,36 +1,36 @@
 import mongoose from "mongoose";
 
 const appointmentSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String },
-  dob: { type: Date, required: true },
-  email: { type: String, required: true },
-  phone: { type: String, required: true },
-  comments: { type: String },
-  marketing: { type: Boolean, default: false },
+    firstName: { type: String, required: true },
+    lastName: { type: String },
+    dob: { type: Date, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    comments: { type: String },
+    marketing: { type: Boolean, default: false },
 
-  appointmentDetails: {
-    practitioner: { type: String, required: true },
-    service: { type: String, required: true },
-    date: { type: String, required: true },
-    time: { type: String, required: true },
-  },
+    appointmentDetails: {
+        practitioner: { type: String, required: true },
+        service: { type: String, required: true },
+        date: { type: String, required: true },
+        time: { type: String, required: true },
+    },
 });
 
 const appointmentModel = mongoose.model("AppointmentModel", appointmentSchema);
 
-appointmentModel.addAppointment = async function ( appointmentData, successCallback, errorCallback) {
-  try {
-    // Create a new instance of the Appointment model
-    const newAppointment = new this(appointmentData);
+appointmentModel.addAppointment = async function (appointmentData, successCallback, errorCallback) {
+    try {
+        // Create a new instance of the Appointment model
+        const newAppointment = new this(appointmentData);
 
-    // Save the appointment to the database
-    const savedAppointment = await newAppointment.save();
+        // Save the appointment to the database
+        const savedAppointment = await newAppointment.save();
 
-    successCallback(savedAppointment);
-  }
-  catch (error) {
-    errorCallback(error);
-  }
+        successCallback(savedAppointment);
+    }
+    catch (error) {
+        errorCallback(error);
+    }
 };
 export default appointmentModel;
