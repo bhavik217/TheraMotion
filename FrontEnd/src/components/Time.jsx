db88ebb57cf186c4f8bca629164179e583c14a35
Bhavik Sharma <bhavik217.be22@chitkara.edu.in>
2024-12-05 21:23:26 +0530

    Created Finalform and fixed bugs in Time component

--- FrontEnd/src/components/Time.jsx
+++ FrontEnd/src/components/Time.jsx
@@ -8,31 +8,31 @@ const Time = ({ date, setdisplay,practitioner,service}) => {
 		setdisplay(true)
 	}
 	return (
-		<div class="timee col-md-7 div2">
+		<div className="timee col-md-7 div2">
 			<h4>Select a time for your session</h4><br />
 
-			<div class="timings">
-				<div class="date">
-					<div class="d">{date}</div>
-					<div class="c" onClick={changedis}><i class="fa-solid fa-xmark"></i></div>
+			<div className="timings">
+				<div className="date">
+					<div className="d">{date}</div>
+					<div className="c" onClick={changedis}><i className="fa-solid fa-xmark"></i></div>
 				</div>
-				<div class="time">
-					<div class="t">
+				<div className="time">
+					<div className="t">
 						<h5>Morning</h5><br />
-						<Link to="/form" state={{practitioner,service,date,time:"9:00 am"}}><button class="ti">9:00 am </button><br /><br /></Link>
-						<Link to="/form" state={{practitioner,service,date,time:"10:00 am"}}><button class="ti">10:00 am </button><br /><br /></Link>
-						<Link to="/form" state={{practitioner,service,date,time:"11:00 am"}}><button class="ti">11:00 am </button></Link>
+						<Link to="/form" state={{practitioner,service,date,time:"9:00 am"}}><button className="ti">9:00 am </button><br /><br /></Link>
+						<Link to="/form" state={{practitioner,service,date,time:"10:00 am"}}><button className="ti">10:00 am </button><br /><br /></Link>
+						<Link to="/form" state={{practitioner,service,date,time:"11:00 am"}}><button className="ti">11:00 am </button></Link>
 					</div>
-					<div class="t">
+					<div className="t">
 						<h5>Afternoon</h5><br />
-						<Link to="/form" state={{practitioner,service,date,time:"1:00 pm"}}><button class="ti">1:00 pm </button><br /><br /></Link>
-						<Link to="/form" state={{practitioner,service,date,time:"2:00 pm"}}><button class="ti">2:00 pm </button></Link>
+						<Link to="/form" state={{practitioner,service,date,time:"1:00 pm"}}><button className="ti">1:00 pm </button><br /><br /></Link>
+						<Link to="/form" state={{practitioner,service,date,time:"2:00 pm"}}><button className="ti">2:00 pm </button></Link>
 
 					</div>
-					<div class="t">
+					<div className="t">
 						<h5>Evening</h5><br />
-						<Link to="/form" state={{practitioner,service,date,time:"5:30 am"}}><button class="ti">5:30 pm </button><br /><br /></Link>
-						<Link to="/form" state={{practitioner,service,date,time:"6:30 am"}}><button class="ti">6:30 pm </button></Link>
+						<Link to="/form" state={{practitioner,service,date,time:"5:30 am"}}><button className="ti">5:30 pm </button><br /><br /></Link>
+						<Link to="/form" state={{practitioner,service,date,time:"6:30 am"}}><button className="ti">6:30 pm </button></Link>
 					</div>
 				</div>
 			</div>
