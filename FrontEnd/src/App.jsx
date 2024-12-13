8a73211ffdd505f518d9a803a12ffafd732af0e0
ARNAV GUPTA <arnavgupta1551@gmail.com>
2024-12-13 21:26:43 +0530

    Installed and imported FontAwesome icons

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "./routes";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 export function App() {
     const router = createBrowserRouter(routes);
