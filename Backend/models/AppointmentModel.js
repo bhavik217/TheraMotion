d694bf208e2e2a775f24eac118143a9b71abab14
Bhavik Sharma <bhavik217.be22@chitkara.edu.in>
2024-12-22 11:26:05 +0530

    Added BookingId logic

--- Backend/models/AppointmentModel.js
+++ Backend/models/AppointmentModel.js
@@ -1,21 +1,22 @@
 import mongoose from "mongoose";
+import Counter from "../utils/counter.js";
 
 const appointmentSchema = new mongoose.Schema({
+    bookingId: {type: Number, required: true, unique: true},
     firstName: { type: String, required: true },
     lastName: { type: String },
     dob: { type: Date, required: true },
     email: { type: String, required: true },
     phone: { type: String, required: true },
     comments: { type: String },
-    marketing: { type: Boolean, default: false },
 
     appointmentDetails: {
         practitioner: { type: String, required: true },
         service: { type: String, required: true },
-        date: { type: String, required: true },
+        date: { type: Date, required: true },
         time: { type: String, required: true },
     },
-});
+}, { timestamps: true });
 
 const appointmentModel = mongoose.model("AppointmentModel", appointmentSchema);
 
@@ -25,6 +26,17 @@ appointmentModel.addAppointment = async function (
     errorCallback
 ) {
     try {
+        // Fetch the next bookingId from the counter
+        const counter = await Counter.findByIdAndUpdate(
+            { _id: "bookingId" },
+            { $inc: { sequenceValue: 1 } },
+            { new: true, upsert: true } // Create counter if it doesn't exist
+        );
+        const nextBookingId = counter.sequenceValue;
+
+        // Add the bookingId to the appointment data
+        appointmentData.bookingId = nextBookingId;
+
         // Create a new instance of the Appointment model
         const newAppointment = new this(appointmentData);
 
