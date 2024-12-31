d5693d69c90df67cd0d2e2184bf824d6e7f979d8
bhavik217 <bhavik217.be22@chitkara.edu.in>
2024-12-31 11:51:33 +0530

    Removed unessential functions

--- Backend/models/AppointmentModel.js
+++ Backend/models/AppointmentModel.js
@@ -23,21 +23,6 @@ const appointmentSchema = new mongoose.Schema(
 
 const appointmentModel = mongoose.model("AppointmentModel", appointmentSchema);
 
-// Helper function to get current time in minutes
-function getCurrentTimeInMinutes() {
-    const now = new Date();
-
-    // Get the UTC time in minutes
-    const utcMinutes = now.getUTCHours() * 60 + now.getUTCMinutes();
-
-    // Add IST offset (5 hours and 30 minutes)
-    const istOffset = 5 * 60 + 30;
-    const istMinutes = (utcMinutes + istOffset + 1440) % 1440; // Ensure result is always positive
-
-    return istMinutes;
-}
-
-
 appointmentModel.addAppointment = async function (
     appointmentData,
     successCallback,
